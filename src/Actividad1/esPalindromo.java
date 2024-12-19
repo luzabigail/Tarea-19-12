26dbd8553ce851dc3687332cfaabeca0226c51b4
Luz <Luz@email.com>
2024-12-19 10:01:43 +0100

    fichero esPalidromo modificado

--- src/Actividad1/esPalindromo.java
+++ src/Actividad1/esPalindromo.java
@@ -1,6 +1,5 @@
 package Actividad1;
-
-public class esPalindromo {
+// Actividad modificada por Luz Bietti 
 	public static boolean validPalindrome(String s) {
 	       for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
 	           if (s.charAt(i) != s.charAt(j)) {
